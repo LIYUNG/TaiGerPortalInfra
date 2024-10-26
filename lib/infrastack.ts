77a79b35d6fd533e054fefce744d9d4089baba62
Leo <liyung.chen@tum.de>
2024-10-26 15:42:29 +0200

    update assumeRole approach

--- lib/infrastack.ts
+++ lib/infrastack.ts
@@ -8,7 +8,7 @@ import {
     ServicePrincipal
 } from "aws-cdk-lib/aws-iam";
 import { Bucket } from "aws-cdk-lib/aws-s3";
-import { tenfoldAIRoleArn } from "../configuration";
+import { AWS_ACCOUNT, tenfoldAIRoleArn } from "../configuration";
 
 interface InfraStackProps extends cdk.StackProps {
     stageName: string;
@@ -29,13 +29,30 @@ export class InfraStack extends cdk.Stack {
             versioned: true,
             removalPolicy: cdk.RemovalPolicy.DESTROY // For testing; in production, consider RETAIN
         });
+        // Create the IAM Role in your account for the external account to assume
+        let assumedBy: CompositePrincipal;
+        if (props.isProd) {
+            assumedBy = new CompositePrincipal(
+                new ServicePrincipal("ec2.amazonaws.com"),
+                new ArnPrincipal(tenfoldAIRoleArn)
+            );
+        } else {
+            assumedBy = new CompositePrincipal(
+                new ArnPrincipal(`arn:aws:iam::${AWS_ACCOUNT}:user/taiger_leo`),
+                new ArnPrincipal(`arn:aws:iam::${AWS_ACCOUNT}:user/taiger_alex`),
+                new ArnPrincipal(`arn:aws:iam::${AWS_ACCOUNT}:user/taiger_abby`)
+            );
+        }
+        const crossAccountRole = new Role(this, "CrossAccountS3AccessRole", {
+            assumedBy,
+            description: "Allows external account IAM role to access S3 bucket objects"
+        });
 
         // 2. Grant GetObject permission for /external-folder/* to the external IAM role
-        bucket.addToResourcePolicy(
+        crossAccountRole.addToPolicy(
             new PolicyStatement({
                 actions: ["s3:GetObject"],
-                resources: [`${bucket.bucketArn}/tenfold-ai-folder/*`],
-                principals: [new ArnPrincipal(tenfoldAIRoleArn)]
+                resources: [`${bucket.bucketArn}/tenfold-ai-folder/*`]
             })
         );
     }
